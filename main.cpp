450f6a2ef4a2094c8fb808f32b17a68904a36de1
naomimurrieta <160088927+naomimurrieta@users.noreply.github.com>
2024-04-30 19:38:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,65 @@ cout << "q - Quit" << endl;
 void ExecuteMenu(char option, ShoppingCart& theCart) {
    /* Type your code here */
 
-
+string temp;
+   double prices;
+   int temp2;
+   switch (option){
+      case 'a': {
+         cout << "ADD ITEM TO CART" << endl;
+         ItemToPurchase newItem;
+         //clear input buffer
+         cin.ignore();
+         court << "Enter the item name: " << endl;
+         getline(cin, temp);
+         newItem.SetName(temp);
+         cout << "Enter the item description: " << endl;
+         getline(cin, temp);
+         newitem.SetDescription(temp);
+         cout<< "Enter the item price: " << endl;
+         cin >> prices;
+         newItem.SetPrice(prices);
+         cout << "Enter the item quantity: " << endl;
+         cin > temp2;
+         newItem.SetQuantity(temp2);
+         theCart.AddItem(newItem);
+         break;
+      }
+      case 'd':{
+         cout << "REMOVE ITEM FROM CART" << endl;
+         string itemName;
+         cin.ignore();
+         cout << "Enter name of item to remove: " << endl;
+         getline(cin, itemName);
+         theCArt.RemoveItem(itemName);
+         break;
+      }
+      case'c':{
+         cout << "CHANGE ITEM QUANTITY" << endl;
+         string itemName;
+         int newQuantity;
+         cin.ignore();
+         cout << "Enter the item name: " << endl;
+         getline(cin, itemName);
+         cout << "Enter the new quantity: " << endl;
+         cin >> newQuantity;
+         ItemToPurchase items(itemName, "none", 0.0, newQuantity);
+         theCart.ModifyItem(items);
+         break;
+      }
+      case'i':
+         cout << "OUTPUT ITEMS' DESCRIPTIONS" << endl;
+         theCart.PrintDescriptions();
+         break;
+      case'o':
+         theCart.PrintTotal();
+         break;
+      case 'q':
+         //cout << "Esiting program..." << endl;
+         break;
+   }
+}
+            
 
 
 
