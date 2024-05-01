1deace208f412c46881bafde3d4f05753eafe6f7
naomimurrieta <160088927+naomimurrieta@users.noreply.github.com>
2024-04-30 19:10:31 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+#include "ItemToPurchase.h"
+using namespace std;
+
+ItemToPurchase::ItemToPurchase(string n, string d,double p, int q){
+name = n;
+itemDescription = d;
+price =p;
+quantity = q;
+}
+
+//Setters
+void ItemToPurchase::SetName(string n){
+  name=n;
+}
+
+void ItemToPurchase::SetDescription(string d){
+  itemDescription = d;
+}
+
+void ItemToPurchase::SetPrice(double p){
+  price = p;
+}
+
+void ItemToPurchase::SetQuantity(int q){
+  quantity = q;
+}
+
+string ItemToPurchase::GetName() const{
+  return name;
+}
+
+string ItemToPurchase::GetDescription() const{
+  return itemDescription;
+}
+
+double ItemToPurchase::GetPrice() const{
+  return price;
+}
+
+int ItemToPurchase::GetQuantity() const{
+  return quantity;
+}
+
+
+
+
+
+
+void ItemToPurchase::PrintItemCost() const{
+  court << name << "" << quantity << "@ $" << price << " = $" << quantity*price << endl;
+}
+void ItemToPurchase::PrintItemDescription() const{
+  court << name << ": " << itemDescription << endl;
+}
 
